1229762bcba810b4809a649403f52b3f12b6890b
Alexis Mauricio Gunsett <mau_gunsett@hotmail.com>
2023-10-24 15:08:54 -0300

    configuro backend

---
+++ .eslintrc.cjs
@@ -0,0 +1,15 @@
+module.exports = {
+    extends: 'airbnb-base',
+    env: {
+      es6: true,
+      browser: true,
+    },
+    rules: {
+      'import/extensions': 'off',
+      camelcase: 'off',
+      'no-restricted-globals': 'off',
+      'import/prefer-default-export': 'off',
+      'no-underscore-dangle': 'off',
+    },
+    ignorePatterns: ['**/node_modules/**', '**/dist/**'],
+  };
